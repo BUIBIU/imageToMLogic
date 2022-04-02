c36b6c93a7c621354b89e3560ab2671a666919e4
BUI <623607878@qq.com>
2022-04-02 12:05:00 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+
+import { Button } from "vant";
+Vue.use(Button);
+
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/views/mainPage/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mainPage">
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    ABCDEFG123456789
+    <m-button></m-button>
+  </div>
+</template>
+
+<script>
+import mButton from "@/components/ui/button";
+export default {
+  components: {
+    mButton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@font-face {
+  font-family: "mindustry-font";
+  src: url("/font/font.woff");
+}
+.mainPage {
+  font-family: "mindustry-font";
+}
+</style>
